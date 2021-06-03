ec7d35e9c85a398f08f10dc8fe71d3cba263062e
moses-mugoya <mosesmugoya31@gmail.com>
2021-06-03 17:49:06 +0300

    updated version 3 landing page

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import { Case } from './pages/caseStatus.jsx';
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { Footer, FooterSection } from './components/footer.jsx'
 import { HeaderSection } from './components/header.jsx'
-import { LandingSection } from './components/landing.jsx'
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <HeaderSection />
-        <LandingSection />
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/case' component={Case} />
