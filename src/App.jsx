36ee60888c657c8c05bc0950bb976969f052cc46
moses-mugoya <mosesmugoya31@gmail.com>
2021-06-05 17:38:00 +0300

    improved the styling

--- src/App.jsx
+++ src/App.jsx
@@ -4,22 +4,22 @@ import { Case } from './pages/caseStatus.jsx';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Footer, FooterSection } from './components/footer.jsx';
 import { HeaderSection } from './components/header.jsx';
-import {Gallery} from './pages/gallery.jsx';
+import { Gallery } from './pages/gallery2.jsx';
 
 
 function App() {
   return (
 
-<BrowserRouter>
-     <div className="App">
-       <HeaderSection />
-     <Switch>
-      <Route exact path='/' component={Home} />
-      <Route exact path='/status' component={Case} />
-      <Route exact path='/gallery' component={Gallery} />
-    </Switch>
-    <FooterSection />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <HeaderSection />
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/status' component={Case} />
+          <Route exact path='/gallery' component={Gallery} />
+        </Switch>
+        <FooterSection />
+      </div>
     </BrowserRouter>
   );
 }
--- src/pages/gallery.jsx
+++ src/pages/gallery.jsx
@@ -1,63 +1,63 @@
 import React from 'react';
 import './styles/gallery.css';
-import {Popover} from 'antd';
+import { Popover } from 'antd';
 
 
-const x= [...Array(20).keys()];
+const x = [...Array(20).keys()];
 
- 
+
 
 const content = (
     <div className="popContent">
         <div className='pop-pic'>
-             <img src="/images/gallery.png" className="pop-pic" />
-            </div>
-    <div className="pop-text">
-        <p>Lorem ipsum dolor sit amet, consectetuer
-        uismod tincidunt ut laoreet dolore magn
-        aliquam erat volutpat. Ut wisi enim ad
-        minim veniam, quis nostrud exerci tation
+            <img src="/images/gallery.png" className="pop-pic" />
+        </div>
+        <div className="pop-text">
+            <p>Lorem ipsum dolor sit amet, consectetuer
+            uismod tincidunt ut laoreet dolore magn
+            aliquam erat volutpat. Ut wisi enim ad
+            minim veniam, quis nostrud exerci tation
         ullamcorper suscipit lobortis nisl ut aliquip</p>
-    </div>
+        </div>
     </div>
 );
 
-export const Gallery = () =>{
-    return(
+export const Gallery = () => {
+    return (
         <>
-        <div className='container'>
-           <div className='header'> <span>Our Gallery</span></div>
-           <div className="gallery-container">
+            <div className='container'>
+                <div className='header'> <span>Our Gallery</span></div>
+                <div className="gallery-container">
 
-            <div className='album'>
-                <span id='album-name'>Album Name</span>
-            <div className="album-container">
-                        {x.map((item,index) =>(
-                    <div className="Card"  key={index.toString()}>
-                        <Popover content={content}  placement="right">
-                             <img src="/images/gallery.png" className='gallery-image' />
-                        </Popover>
-                        </div> ))} 
-                </div>
+                    <div className='album'>
+                        <span id='album-name'>Album Name</span>
+                        <div className="album-container">
+                            {x.map((item, index) => (
+                                <div className="Card" key={index.toString()}>
+                                    <Popover content={content} placement="right">
+                                        <img src="/images/gallery.png" className='gallery-image' />
+                                    </Popover>
+                                </div>))}
+                        </div>
 
-                </div>
-                 
-                <div className='album'>
-                <span id='album-name'>Album Name</span>
-            <div className="album-container">
-                        {x.map((item,index) =>(
-                    <div className="Card"  key={index.toString()}>
-                        <Popover content={content}  placement="right">
-                             <img src="/images/gallery.png" className='gallery-image' />
-                        </Popover>
-                        </div> ))} 
-                </div>
+                    </div>
 
-                </div>
+                    <div className='album'>
+                        <span id='album-name'>Album Name</span>
+                        <div className="album-container">
+                            {x.map((item, index) => (
+                                <div className="Card" key={index.toString()}>
+                                    <Popover content={content} placement="right">
+                                        <img src="/images/gallery.png" className='gallery-image' />
+                                    </Popover>
+                                </div>))}
+                        </div>
+
+                    </div>
 
                 </div>
             </div>
-            
+
         </>
     )
 }
