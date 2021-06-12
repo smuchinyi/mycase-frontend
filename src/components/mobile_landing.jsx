713a829b37d69eaecff75e8fb2d5b627702d7976
moses-mugoya <mosesmugoya31@gmail.com>
2021-06-12 14:53:21 +0300

    added responsiveness to the header, footer and landing page

--- src/components/mobile_landing.jsx
+++ src/components/mobile_landing.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment, useEffect, useState } from 'react'
 import child from '../images/child-fl4.png'
 import footer from '../images/footer.png'
-import "./landing.css";
+import "./mobile_landing.css";
 import { Row, Col, Modal } from 'antd';
 import "antd/dist/antd.css";
 import { useTranslation } from 'react-i18next'
@@ -12,33 +12,32 @@ import Button from '@material-ui/core/Button';
 
 
 const row1 = {
-    height: "700px",
     position: "relative"
 }
 
 const row2 = {
-    height: "500px",
     baackgroundColor: "white",
-    padding: "50px"
+    padding: "50px",
+    marginTop: "370px"
 }
 
 const row3 = {
-    height: "700px",
 }
 
 const form = {
-    height: "500px",
-    width: "500px",
+    height: "530px",
     backgroundColor: "#ea9b0a",
+    width: "100%",
     position: "absolute",
     bottom: "0px",
-    top: "350px",
-    right: "40px",
+    top: "200px",
+    right: "auto",
+    left: "auto",
     padding: "20px"
 }
 
 
-export const LandingSection = () => {
+export const MobileLandingSection = () => {
     const [isModalVisible, setIsModalVisible] = useState(false);
 
     const showModal = () => {
@@ -63,14 +62,14 @@ export const LandingSection = () => {
     return (
         <>
             <div style={row1}>
-                <img src={child} width="100%" height="700px" className="bg-image" />
-                <h1 className="image-text1">MyCase</h1>
-                <h1 className="image-text2">US Refugee Admissions</h1>
-                <h1 className="image-text3">Program</h1>
+                <img src={child} width="100%" className="bg-image" />
+                <h1 className="mobile-image-text1">MyCase</h1>
+                <h1 className="mobile-image-text2">US Refugee Admissions</h1>
+                <h1 className="mobile-image-text3">Program</h1>
 
-                <p className="image-text4">Lorem ipsum dolor sit amet, consec</p>
-                <p className="image-text5">tetuer adipiscing elit, sed diam nonum</p>
-                <p className="image-text6">my nibh euismod tincidunt ut laoreet</p>
+                <p className="mobile-image-text4">Lorem ipsum dolor sit amet, consec</p>
+                <p className="mobile-image-text5">tetuer adipiscing elit, sed diam nonum</p>
+                <p className="mobile-image-text6">my nibh euismod tincidunt ut laoreet</p>
 
                 <div style={form}>
                     <h1 style={{ fontWeight: "bold", textAlign: "center", fontSize: "40px" }}>{t('check_my_case')}</h1>
@@ -158,20 +157,20 @@ export const LandingSection = () => {
             </div>
 
             <Row style={row3} gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                <Col span={16} style={{ backgroundColor: "#353582", padding: "50px", textAlign: "center" }}>
-                    <h1 style={{ color: "white", fontWeight: "bold", fontSize: "45px" }}>Notice</h1>
-                    <p style={{ color: "white", fontSize: "18px" }}>The U.S. Government and RSC Africa, operated by CWS, are dedicated to ensuring the safety
+                <Col span={24} style={{ backgroundColor: "#353582", padding: "50px" }}>
+                    <h1 style={{ color: "white", fontWeight: "bold", fontSize: "45px", textAlign: "center" }}>Notice</h1>
+                    <p style={{ color: "white", fontSize: "15px", margin: "10px" }}>The U.S. Government and RSC Africa, operated by CWS, are dedicated to ensuring the safety
                     and security of refugee applicants and our staff around the world who support the U.S. Refugee
                     Admissions Program (USRAP). For the latest information regarding COVID-19 and how to best
                     protect yourself and your family, please visit the Centers for Disease Control and Prevention’s
 (CDC) website: https://www.cdc.gov/coronavirus/2019-ncov/index.html.</p>
-                    <p style={{ color: "white", fontSize: "18px" }}>RSC Africa continues to process cases for departure that conform to the FY 2021 Presidential
+                    <p style={{ color: "white", fontSize: "15px", margin: "10px" }}>RSC Africa continues to process cases for departure that conform to the FY 2021 Presidential
                     Determination (PD) on Refugee Admissions. Cases that do not fall under one of the categories
                     in the PD will not be able to depart to the United States at this time. These cases will remain
                     open and the RSC will reach out to the applicant if there is any action needed on the case.
                     Given the changes to the USRAP program categories this fiscal year, we urge applicants to
 remain patient.</p>
-                    <p style={{ color: "white", fontSize: "18px" }}>The RSC is continuing to operate at reduced levels, and has temporarily postponed in-person
+                    <p style={{ color: "white", fontSize: "15px", margin: "10px" }}>The RSC is continuing to operate at reduced levels, and has temporarily postponed in-person
                     processing and appointments (if applicable) for the safety of staff and refugee clients. Please
                     check our website at mycase.rscafrica.org for general updates on USRAP processing and RSC
                     operations. As updates for your specific case become available, we will inform you. In the
@@ -179,9 +178,9 @@ remain patient.</p>
                     the volume of inquiries may be high, there may be delays and we kindly ask for your patience in
 addressing non-urgent matters.</p>
                 </Col>
-                <Col span={8} style={{ backgroundColor: "#353582" }}>
+                {/* <Col span={8} style={{ backgroundColor: "#353582" }}>
                     <img src={footer} width="100%" height="700px" style={{ objectFit: "cover" }} />
-                </Col>
+                </Col> */}
             </Row>
         </>
     )
