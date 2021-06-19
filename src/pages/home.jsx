c84ad7e4cb54ba53573fed16df11c5dda7b8a84f
moses-mugoya <mosesmugoya31@gmail.com>
2021-06-19 12:17:44 +0300

    fixed bugs

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { LandingSection } from '../components/landing'
 import { MobileLandingSection } from '../components/mobile_landing'
+import { MediumLandingSection } from '../components/medium_landing';
 import { useTheme } from '@material-ui/core/styles';
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 
@@ -11,10 +12,12 @@ import useMediaQuery from '@material-ui/core/useMediaQuery';
 export const Home = () => {
     const theme = useTheme();
     const matches = useMediaQuery(theme.breakpoints.down('sm'));
+    const matches_medium = useMediaQuery(theme.breakpoints.up('sm'))
+    const matches_large = useMediaQuery(theme.breakpoints.down('md'))
     return (
         <>
             {
-                matches ? <MobileLandingSection /> : <LandingSection />
+                matches ? <MobileLandingSection /> : matches_medium && matches_large ? <MediumLandingSection /> : <LandingSection />
             }
         </>
     )
